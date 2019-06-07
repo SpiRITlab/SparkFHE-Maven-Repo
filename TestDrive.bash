9e92c5709b01d60dcc9e3dac9b30cd9362902cb2
Peizhao Hu <ph@cs.rit.edu>
2019-06-07 12:59:04 -0400

    update TestDrive script

--- TestDrive.bash
+++ TestDrive.bash
@@ -177,17 +177,33 @@ fi
 
 
 # add to PATH variable
-bashrc_changed=false
 if [[ "$(grep $Spark_Distribution_Name ~/.bashrc)" == "" ]] ; then 
-	echo "export PATH=$Current_Directory/$Spark_Distribution_Name/bin:"'$PATH' >> ~/.bashrc
-	bashrc_changed=true
-fi
-if [[ "$(grep hadoop ~/.bashrc)" == "" ]] ; then 
-	echo "export PATH=$Current_Directory/$Spark_Distribution_Name/hadoop/bin:$Current_Directory/$Spark_Distribution_Name/hadoop/sbin:"'$PATH' >> ~/.bashrc
-	bashrc_changed=true
-fi
+	echo "
+	export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+	export SPARKFHE_HOME=/spark-3.0.0-SNAPSHOT-bin-SparkFHE
+	
+	# Hadoop Environment Variables
+	export HADOOP_HOME=$SPARKFHE_HOME/hadoop
+	export HADOOP_PREFIX=$HADOOP_HOME
+	export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+	export HADOOP_MAPRED_HOME=$HADOOP_HOME
+	export HADOOP_COMMON_HOME=$HADOOP_HOME
+	export HADOOP_HDFS_HOME=$HADOOP_HOME
+	export YARN_HOME=$HADOOP_HOME
+	export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
 
-if [ "$bashrc_changed" = true ] ; then
+	export HDFS_NAMENODE_USER=\"root\"
+	export HDFS_DATANODE_USER=\"root\"
+	export HDFS_SECONDARYNAMENODE_USER=\"root\"
+	export YARN_RESOURCEMANAGER_USER=\"root\"
+	export YARN_NODEMANAGER_USER=\"root\"
+
+	# Hadoop native path
+	export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+	export HADOOP_OPTS=\"-Djava.library.path=$HADOOP_HOME/lib\"
+
+	# SparkFHE Environment Variables
+	export PATH=/$SPARKFHE_HOME/bin:$PATH" >> ~/.bashrc
 	source ~/.bashrc
 fi
 
