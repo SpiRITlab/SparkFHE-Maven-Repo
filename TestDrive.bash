f3d54f8e85a5f4ce41962e3fbdbea7bfb9c6c3a4
Peizhao Hu <ph@cs.rit.edu>
2019-06-07 13:02:15 -0400

    update TestDrive script

--- TestDrive.bash
+++ TestDrive.bash
@@ -149,6 +149,39 @@ function fetch_shared_libraries() {
 	echo "DONE"
 }
 
+function update_environment_variables() {
+	# add to PATH variable
+	if [[ "$(grep $Spark_Distribution_Name ~/.bashrc)" == "" ]] ; then 
+		echo "
+			export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+			export SPARKFHE_HOME=/spark-3.0.0-SNAPSHOT-bin-SparkFHE
+			
+			# Hadoop Environment Variables
+			export HADOOP_HOME=$SPARKFHE_HOME/hadoop
+			export HADOOP_PREFIX=$HADOOP_HOME
+			export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
+			export HADOOP_MAPRED_HOME=$HADOOP_HOME
+			export HADOOP_COMMON_HOME=$HADOOP_HOME
+			export HADOOP_HDFS_HOME=$HADOOP_HOME
+			export YARN_HOME=$HADOOP_HOME
+			export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
+
+			export HDFS_NAMENODE_USER=\"root\"
+			export HDFS_DATANODE_USER=\"root\"
+			export HDFS_SECONDARYNAMENODE_USER=\"root\"
+			export YARN_RESOURCEMANAGER_USER=\"root\"
+			export YARN_NODEMANAGER_USER=\"root\"
+
+			# Hadoop native path
+			export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
+			export HADOOP_OPTS=\"-Djava.library.path=$HADOOP_HOME/lib\"
+
+			# SparkFHE Environment Variables
+			export PATH=/$SPARKFHE_HOME/bin:$PATH" >> ~/.bashrc
+		source ~/.bashrc
+	fi
+}
+
 
 CheckCommands
 
@@ -166,46 +199,20 @@ elif [[ "$PackageName" == "addon" ]]; then
 	fetch_sparkfhe_addon
 elif [[ "$PackageName" == "lib" ]]; then
 	fetch_shared_libraries
+elif [[ "$PackageName" == "variables" ]]; then
+	update_environment_variables
 elif [[ "$PackageName" == "all" ]]; then
 	fetch_spark_distribution
 	fetch_hadoop_distribution $ExtraArg
 	fetch_dependencies
 	fetch_sparkfhe_addon
 	fetch_shared_libraries
+	update_environment_variables
 fi
 
 
 
-# add to PATH variable
-if [[ "$(grep $Spark_Distribution_Name ~/.bashrc)" == "" ]] ; then 
-	echo "
-	export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
-	export SPARKFHE_HOME=/spark-3.0.0-SNAPSHOT-bin-SparkFHE
-	
-	# Hadoop Environment Variables
-	export HADOOP_HOME=$SPARKFHE_HOME/hadoop
-	export HADOOP_PREFIX=$HADOOP_HOME
-	export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
-	export HADOOP_MAPRED_HOME=$HADOOP_HOME
-	export HADOOP_COMMON_HOME=$HADOOP_HOME
-	export HADOOP_HDFS_HOME=$HADOOP_HOME
-	export YARN_HOME=$HADOOP_HOME
-	export PATH=$PATH:$HADOOP_HOME/bin:$HADOOP_HOME/sbin
 
-	export HDFS_NAMENODE_USER=\"root\"
-	export HDFS_DATANODE_USER=\"root\"
-	export HDFS_SECONDARYNAMENODE_USER=\"root\"
-	export YARN_RESOURCEMANAGER_USER=\"root\"
-	export YARN_NODEMANAGER_USER=\"root\"
-
-	# Hadoop native path
-	export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native
-	export HADOOP_OPTS=\"-Djava.library.path=$HADOOP_HOME/lib\"
-
-	# SparkFHE Environment Variables
-	export PATH=/$SPARKFHE_HOME/bin:$PATH" >> ~/.bashrc
-	source ~/.bashrc
-fi
 
 echo "The SparkFHE environment is all set. Enjoy!"
 
