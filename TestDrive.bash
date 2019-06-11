5252f8b64b8150499d4b6e11e7dec6c04a888601
Peizhao Hu <peiworld@gmail.com>
2019-06-11 00:43:12 -0400

    [2019-06-11 00:43:04] Update api package(s)

--- TestDrive.bash
+++ TestDrive.bash
@@ -16,7 +16,7 @@ libSparkFHEName="libSparkFHE"
 
 ### UPDATE AUTOMATICALLY by running deploy.bash
 SparkFHE_Plugin_latest_jar_file=spiritlab/sparkfhe/spark-fhe_2.12/1.0-SNAPSHOT/spark-fhe_2.12-1.0-20190528.232552-1-jar-with-dependencies.jar
-SparkFHE_API_latest_jar_file=spiritlab/sparkfhe/sparkfhe-api/1.0-SNAPSHOT/sparkfhe-api-1.0-20190611.024334-1.jar
+SparkFHE_API_latest_jar_file=spiritlab/sparkfhe/sparkfhe-api/1.0-SNAPSHOT/sparkfhe-api-1.0-20190611.044311-1.jar
 SparkFHE_Examples_latest_jar_file=spiritlab/sparkfhe/sparkfhe-examples/1.0-SNAPSHOT/sparkfhe-examples-1.0-20190611.025724-1.jar
 #######################################
 Current_Directory=`pwd`
